657db28523a4fa9efe7e6c242ee66e67d0c03e51
Голиков Иван Вадимович <igolikov@octoberry.ru>
2020-05-04 13:43:48 +0300

    Updated stemming worker

--- stemming/StemmWorker.hs
+++ stemming/StemmWorker.hs
@@ -1,4 +1,6 @@
 module Stemming
 
 stem :: String -> String -> String -> Int -> String
-stem firstWord secondWord result offset = ""
+stem firstWord secondWord result offset = [secondChar, firstChar]
+    where firstChar = firstWord!!offset
+          secondChar = secondWord!!offset
