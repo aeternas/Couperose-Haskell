850be5f3b84c3245de1af051d86f61ff9046f4fc
Голиков Иван Вадимович <igolikov@octoberry.ru>
2020-05-04 20:19:53 +0300

    Removed `show`

--- Main.hs
+++ Main.hs
@@ -8,4 +8,4 @@ combine (x:xs) (y:ys) = (x ++ y) : combine xs ys
 main = do
   putStrLn "Enter words to stem"
   words' <- getLine
-  putStrLn ( show (stem words' "" 0) )
+  putStrLn ( (stem words' "" 0) )
