8ffcc6935b459f42920bc8d13e2acf6d8ffc6009
Голиков Иван Вадимович <igolikov@octoberry.ru>
2020-05-04 13:59:06 +0300

    Fixed stemming module

--- stemming/StemmWorker.hs
+++ stemming/StemmWorker.hs
@@ -1,6 +1,10 @@
-module Stemming
+module Stemming where
 
 stem :: String -> String -> String -> Int -> String
-stem firstWord secondWord result offset = [secondChar, firstChar]
+stem firstWord secondWord result offset
+    | firstChar == secondChar = stem firstWord secondWord finalResult finalOffset
+    | otherwise = result
     where firstChar = firstWord!!offset
           secondChar = secondWord!!offset
+          finalResult = result ++ firstChar:[]
+          finalOffset = offset + 1
