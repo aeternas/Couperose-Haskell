edadf54efcd1fbdab4de8052672d7e874f4232c4
Голиков Иван Вадимович <igolikov@octoberry.ru>
2020-05-04 20:10:54 +0300

    Added disabled "smarter" test

--- test/Tests.hs
+++ test/Tests.hs
@@ -6,5 +6,10 @@ testStemming =
     TestCase $ assertEqual "Should return `consult`"
                            "consult" (stem "consulting consultant" "" 0)
 
+testStemmingFirstWordDiscrepancy :: Test
+testStemmingFirstWordDiscrepancy =
+    TestCase $ assertEqual "Should return `ungar`"
+                           "ungar" (stem "hungary ungarn" "" 0)
+
 main :: IO Counts
 main = runTestTT $ TestList [testStemming]
